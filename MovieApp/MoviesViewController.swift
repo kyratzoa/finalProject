5cb93c68509ce6307966cc9bf7b81cf4e4d0f888
Anastasia Kyrazoglou <arkyratzog@gmail.com>
2019-12-07 13:17:20 -0500

    Firebase Update

--- MovieApp/MoviesViewController.swift
+++ MovieApp/MoviesViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 
 class MoviesViewController: UIViewController {
@@ -15,10 +17,14 @@ class MoviesViewController: UIViewController {
     
     var movies = Movies()
     var activityIndicator = UIActivityIndicatorView()
+    var authUI: FUIAuth!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        authUI = FUIAuth.defaultAuthUI()
+        auth?.delegate = self
+        
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
@@ -134,5 +140,45 @@ extension UIImageView {
     func dowloadFromServer(link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
         guard let url = URL(string: link) else { return }
         dowloadFromServer(url: url, contentMode: mode)
+    }
+}
+
+extension TeamListViewController: FUIAuthDelegate {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        if let user = user {
+            // Assumes data will be isplayed in a tableView that was hidden until login was verified so unauthorized users can't see data.
+            tableView.isHidden = false
+            print("^^^ We signed in with the user \(user.email ?? "unknown e-mail")")
+        }
+    }
+    func authPickerViewController(forAuthUI authUI: FUIAuth) -> FUIAuthPickerViewController {
+        
+        // Create an instance of the FirebaseAuth login view controller
+        let loginViewController = FUIAuthPickerViewController(authUI: authUI)
+        
+        // Set background color to white
+        loginViewController.view.backgroundColor = UIColor.white
+        
+        // Create a frame for a UIImageView to hold our logo
+        let marginInsets: CGFloat = 16 // logo will be 16 points from L and R margins
+        let imageHeight: CGFloat = 225 // the height of our logo
+        let imageY = self.view.center.y - imageHeight // places bottom of UIImageView in the center of the login screen
+        let logoFrame = CGRect(x: self.view.frame.origin.x + marginInsets, y: imageY, width: self.view.frame.width - (marginInsets*2), height: imageHeight)
+        
+        // Create the UIImageView using the frame created above & add the "logo" image
+        let logoImageView = UIImageView(frame: logoFrame)
+        logoImageView.image = UIImage(named: "logo")
+        logoImageView.contentMode = .scaleAspectFit // Set imageView to Aspect Fit
+        loginViewController.view.addSubview(logoImageView) // Add ImageView to the login controller's main view
+        return loginViewController
     }
 }
--- Podfile
+++ Podfile
@@ -1,11 +1,12 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '11.0'
+platform :ios, '12.0'
 use_frameworks!
 
 target 'MovieApp' do
     pod 'Alamofire', '~> 4.9'
     pod 'SwiftyJSON', '~> 4.0'
     pod 'AlamofireImage', '~> 3.5'
+
     pod 'Firebase/Core'
     pod 'Firebase/Storage'
     pod 'Firebase/Firestore'
@@ -13,6 +14,5 @@ target 'MovieApp' do
     pod 'FirebaseUI/Email'
     pod 'FirebaseUI/Auth'
     pod 'FirebaseUI/Google'
-    pod 'SDWebImage', '~> 4.0'
 
 end
